57080d3c7e8df1771c8949cb68c9c29cedaec159
baboucarr <baboucarrd43@gmail.com>
2019-04-11 14:13:01 +0000

    fixed contact pages

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,5 @@
-// let navContainer = document.getElementById("nav")
 
-// var links = navContainer.getElementsByClassName("link");
-// for (var i = 0; i < links.length; i++) {
-//     links[i].addEventListener("click", function() {
-//         var current = document.getElementsByClassName("active");
-//         current[0].className = current[0].className.replace("active", "");
-//         this.className += "active";
-//     })
-// }
+
 
 function myFunction(e) {
     var elems = document.querySelector(".active");
